c19ffaba087a58808b5c5ee2df884b3e9d8cf405
linhgiac <khanhly.hcmut@gmail.com>
2022-12-30 17:48:51 +0700

    fe(export): download button

--- front-end/components/page/resume/resume-export/resume-export-selection/index.tsx
+++ front-end/components/page/resume/resume-export/resume-export-selection/index.tsx
@@ -5,6 +5,7 @@ import jsPDF from 'jspdf';
 import React from 'react';
 import { useRecoilValue } from 'recoil';
 import { resumeSavedState } from '../../../../../recoil-state/resume-state/resume.state';
+import DownloadButton from '../../../../custom/downnload-button';
 import styles from './styles.module.scss';
 
 type ResumeExportSelectionProps = {
@@ -14,31 +15,8 @@ type ResumeExportSelectionProps = {
 
 const ResumeExportSelection = (props: ResumeExportSelectionProps) => {
     const { className, onChangeEditorLayout } = props;
-    const resume = useRecoilValue(resumeSavedState);
     const clickHandler = () => {
         onChangeEditorLayout();
-    };
-    const downloadHandler = async () => {
-        const pdf = new jsPDF('portrait', 'pt', 'a4');
-        const data: any = await document.querySelector('#pdf');
-        const width = pdf.internal.pageSize.getWidth();
-        if (data) {
-            pdf.html(data, { x: 0, y: 0, width: width }).then(() => {
-                pdf.save(`${resume.title}.pdf`);
-            });
-        }
-
-        // const input: any = document.getElementById('pdf');
-
-        // html2canvas(input).then(canvas => {
-        //     const imgData = canvas.toDataURL('image/png');
-        //     const pdf = new jsPDF();
-        //     const width = pdf.internal.pageSize.getWidth();
-        //     const height = pdf.internal.pageSize.getHeight();
-        //     pdf.addImage(imgData, 'JPEG', 0, 0, width, height);
-        //     // pdf.output('dataurlnewwindow');
-        //     pdf.save('download.pdf');
-        // });
     };
     return (
         <div className={classNames(className, styles['resume-export-button'])}>
@@ -50,12 +28,7 @@ const ResumeExportSelection = (props: ResumeExportSelectionProps) => {
                 className={styles['resume-export-button__selection']}>
                 Select Template
             </Button>
-            <Button
-                type="primary"
-                size="large"
-                onClick={downloadHandler}>
-                Download
-            </Button>
+            <DownloadButton />
         </div>
     );
 };
