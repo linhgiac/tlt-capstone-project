fafdd10f76e5c0835d66f2103819909353855ad2
linhgiac <khanhly.hcmut@gmail.com>
2023-01-07 23:38:13 +0700

    fe(editor): fix bug

--- front-end/pages/_app.tsx
+++ front-end/pages/_app.tsx
@@ -4,18 +4,22 @@ import { ConfigProvider } from 'antd';
 import { RecoilRoot } from 'recoil';
 import Layout from '../components/layout';
 import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 interface MyAppPropsType {
     currentLayout: string;
 }
 function MyApp({ Component, pageProps }: AppProps<MyAppPropsType>) {
     const { currentLayout } = pageProps;
-
+    const router = useRouter();
     return (
         <ConfigProvider prefixCls="tlt">
             <RecoilRoot>
                 <Layout currentLayout={currentLayout}>
-                    <Component {...pageProps} />
+                    <Component
+                        key={router.asPath}
+                        {...pageProps}
+                    />
                 </Layout>
             </RecoilRoot>
         </ConfigProvider>
