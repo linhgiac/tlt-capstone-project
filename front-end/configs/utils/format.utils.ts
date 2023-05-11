5a4b7848c5e75357040f0e2060e2544ae160cb55
ThaiTran31 <thaitrancs31@gmail.com>
2023-05-11 22:30:36 +0700

    hot fix CORS config images

--- front-end/configs/utils/format.utils.ts
+++ front-end/configs/utils/format.utils.ts
@@ -246,12 +246,10 @@ export const convertProfilePayloadData = (payloadData: AccountSettingType) => {
 
 export const convertProfileResponse = (responseData: any) => {
     const { profile, ...dataWithoutProfile } = responseData;
-    if (profile?.avatar) {
-        profile.avatar = `${HOST}${profile.avatar.replace('/', '')}`;
-    }
+
     return convertSnakeToCamel({
-        ...dataWithoutProfile,
         ...profile,
+        ...dataWithoutProfile
     });
 };
 
