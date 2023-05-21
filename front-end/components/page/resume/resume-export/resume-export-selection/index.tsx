04b0c16f6c7afca54b46580de7a5b79cb2bbbdcf
Toàn. Phan Đinh Minh <pdminhtoan@gmail.com>
2023-05-21 15:04:14 +0700

    Add AI

--- front-end/components/page/resume/resume-export/resume-export-selection/index.tsx
+++ front-end/components/page/resume/resume-export/resume-export-selection/index.tsx
@@ -32,6 +32,7 @@ const ResumeExportSelection = (props: ResumeExportSelectionProps) => {
             for (let i = 0; i < keys.length; i++) {
                 const key = keys[i];
                 if (key === 'sectionType') continue;
+                if (key === 'isShownLevel') continue;
                 if (obj.hasOwnProperty(key) && other.hasOwnProperty(key)) {
                     if (
                         typeof obj[key] === 'object' &&
